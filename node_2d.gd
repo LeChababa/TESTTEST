687df2894c20f7fc147f044ccfa24c01664afb94
LeChababa <miakyshvlad@gmail.com>
2026-03-25 07:50:52 +0200

    TttTTtTttT

--- node_2d.gd
+++ node_2d.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 
 func _ready() -> void:
 	print("F")
+
+func _process(delta: float) -> void:
+	sprite_2d.rotation_degrees += 1.0 * delta
+
+func _kill_yura(i: int) -> void:
+	if i == 0:
+		print("kill Yura")
